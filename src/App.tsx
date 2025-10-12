800b699c67ec689fe75ee8db26f01171701c0139
Pablo F. Moura <pablo.f.moura17@gmail.com>
2025-10-12 20:24:08 -0300

    💡 Feature: Implementation of a new feature or functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,173 @@
-import { CONTACTS, SKILLS } from "./constants/app";
+import { Button, Typography } from "@mui/material";
+import { CONTACTS, SKILLS, WORKS } from "./constants/app";
 import profile from "./assets/profile.jpg";
 
 function App() {
   return (
     <div
       style={{
-        flex: 1,
-        border: "1px solid #ccc",
-        width: "90vw",
-        maxWidth: "1000px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        margin: "auto",
         padding: "20px",
-        borderRadius: "10px",
+        display: "flex",
+        minWidth: "100vw",
+        maxWidth: "100vw",
+        overflow: "hidden",
+        minHeight: "100vh",
+        maxHeight: "100vh",
+        boxSizing: "border-box",
       }}
     >
-      <section style={{ textAlign: "center", marginBottom: 40 }}>
-        <img
-          src={profile}
-          alt="Avatar"
+      <div
+        style={{
+          gap: "12px",
+          flex: 1,
+          border: "1px solid #ccc",
+          display: "flex",
+          padding: "20px",
+          minWidth: "calc(100vw - 40px)",
+          maxWidth: "calc(100vw - 40px)",
+          overflow: "auto",
+          minHeight: "calc(100vh - 40px)",
+          maxHeight: "calc(100vh - 40px)",
+          alignItems: "center",
+          borderRadius: "10px",
+          flexDirection: "column",
+          backgroundColor: "#f9f9f9",
+        }}
+      >
+        <section
           style={{
-            width: 120,
-            height: 120,
-            borderRadius: "50%",
-            marginBottom: 16,
-          }}
-        />
-        <h1>Pablo Moura</h1>
-        <h2 style={{ color: "#646cff", fontWeight: 400 }}>Programador</h2>
-        <p>Bem-vindo ao meu portfólio!</p>
-        <p>
-          Aqui você encontra minhas principais habilidades, trabalhos e formas
-          de contato.
-        </p>
-      </section>
-      <section>
-        <h3>Habilidades</h3>
-        <ul
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: 12,
-            listStyle: "none",
-            padding: 0,
+            textAlign: "center",
+            marginBottom: 40,
           }}
         >
-          {SKILLS.map((skill) => (
-            <li
-              key={skill}
-              style={{
-                background: "#646cff22",
-                color: "#646cff",
-                padding: "8px 16px",
-                borderRadius: 8,
-                fontWeight: 500,
-              }}
-            >
-              {skill}
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section style={{ marginTop: 40, textAlign: "center" }}>
-        {CONTACTS.map((contact) => (
-          <a
-            key={contact.name}
-            href={contact.link}
-            target="_blank"
-            rel="noopener noreferrer"
+          <img
+            src={profile}
+            alt="Avatar"
             style={{
-              marginRight: 16,
-              textTransform: "capitalize",
+              width: 120,
+              height: 120,
+              borderRadius: "50%",
+              marginBottom: 16,
+            }}
+          />
+          <h1>Pablo Moura</h1>
+          <h2 style={{ color: "#646cff", fontWeight: 400 }}>Programador</h2>
+          <p>Bem-vindo ao meu portfólio!</p>
+          <p>
+            Aqui você encontra minhas principais habilidades, trabalhos e formas
+            de contato.
+          </p>
+        </section>
+
+        <section
+          style={{
+            flex: 1,
+            width: "100%",
+            border: "1px solid #ccc",
+            padding: "12px",
+            maxWidth: "1000px",
+            borderRadius: "10px",
+            backgroundColor: "#edeaeaff",
+          }}
+        >
+          <h3>Habilidades</h3>
+          <ul
+            style={{
+              gap: 12,
+              display: "flex",
+              padding: 0,
+              flexWrap: "wrap",
+              listStyle: "none",
             }}
           >
-            <img
-              src={contact.image}
-              alt={contact.name}
+            {SKILLS.map((skill) => (
+              <li
+                key={skill}
+                style={{
+                  color: "#646cff",
+                  padding: "8px 16px",
+                  background: "#646cff22",
+                  fontWeight: 500,
+                  borderRadius: 8,
+                }}
+              >
+                {skill}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section
+          style={{
+            flex: 1,
+            width: "100%",
+            border: "1px solid #ccc",
+            padding: "12px",
+            maxWidth: "1000px",
+            borderRadius: "10px",
+            backgroundColor: "#edeaeaff",
+          }}
+        >
+          <h3>Trabalhos</h3>
+          <ul
+            style={{
+              gap: 12,
+              display: "flex",
+              padding: 0,
+              flexWrap: "wrap",
+              listStyle: "none",
+              flexDirection: "column",
+            }}
+          >
+            {WORKS.map((work) => (
+              <div
+                key={work.name}
+                style={{
+                  gap: "12px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "start",
+                  padding: "12px",
+                  background: "#dadae5ff",
+                  fontWeight: 500,
+                  borderRadius: 8,
+                }}
+              >
+                <Button variant="contained">{work.name}</Button>
+                <Typography color="text.primary">{work.description}</Typography>
+                <Typography color="text.primary">{work.information}</Typography>
+              </div>
+            ))}
+          </ul>
+        </section>
+
+        <section style={{ marginTop: "auto", textAlign: "center" }}>
+          {CONTACTS.map((contact) => (
+            <a
+              key={contact.name}
+              rel="noopener noreferrer"
+              href={contact.link}
               style={{
-                width: 24,
-                height: 24,
-                marginRight: 8,
-                backgroundColor: "red",
+                marginRight: 16,
+                textTransform: "capitalize",
               }}
-            />
-          </a>
-        ))}
-      </section>
+              target="_blank"
+            >
+              <img
+                src={contact.image}
+                alt={contact.name}
+                style={{
+                  width: 45,
+                  height: 45,
+                  marginRight: 8,
+                  backgroundColor: "red",
+                }}
+              />
+            </a>
+          ))}
+        </section>
+      </div>
     </div>
   );
 }
--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,4 +1,4 @@
-import type { TypeContact } from "../types/app";
+import type { TypeContact, TypeWork } from "../types/app";
 import emailImage from "../assets/email.jpg";
 import gitHubImage from "../assets/gitHub.png";
 import whatsappImage from "../assets/whatsapp.jpg";
@@ -43,5 +43,16 @@ export const CONTACTS: TypeContact[] = [
     name: "linkedIn",
     link: "https://www.linkedin.com/in/pablo-francisco-moura/",
     image: linkedInImage,
+  },
+] as const;
+
+export const WORKS: TypeWork[] = [
+  {
+    name: "Gestão Financeira",
+    link: "https://finance-manager-eight-beta.vercel.app",
+    description: "Plataforma de gestão financeira para controle de despesas.",
+    image:
+      "https://logodownload.org/wp-content/uploads/2019/07/easynvest-logo-1.png",
+    information: "Para logar use: user: admin | password: 123",
   },
 ] as const;
