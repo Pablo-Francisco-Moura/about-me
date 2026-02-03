e59a5cfee6a3656101341fda87397a5cc61ac084
Pablo F. Moura <pablo.f.moura17@gmail.com>
2026-02-03 18:29:33 -0300

    🛠️  Setting: Settings.

---
+++ src/i18n.ts
@@ -0,0 +1,24 @@
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import pt from "./locales/pt.json";
+import en from "./locales/en.json";
+
+const resources = {
+  pt: { translation: pt },
+  en: { translation: en },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { CssBaseline } from "@mui/material";
+import { I18nextProvider } from "react-i18next";
 import App from "./App.tsx";
+import i18n from "./i18n";
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CssBaseline />
-    <App />
-  </StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </StrictMode>,
 );
