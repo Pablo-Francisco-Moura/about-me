d9b1d106c5edbb2b7f2401aaa40dd5c67726dd18
Pablo F. Moura <pablo.f.moura17@gmail.com>
2026-08-05 21:53:32 -0300

    ✨ Feature: Centralize copyright metadata

---
+++ src/types/copyright.ts
@@ -0,0 +1,9 @@
+export type TypeCopyright = {
+  year: 2025; // Fixed year, project start date.
+  version: string;
+  firstName: string;
+  lastName: string;
+  developedBy: string;
+  lastUpdated: string;
+  fullLastName: string;
+};
