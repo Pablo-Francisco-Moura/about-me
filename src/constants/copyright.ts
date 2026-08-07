a12f4f3f1d085c1bbe37fe0f791b8dc8c93bf8d5
Pablo Moura <pablo.moura@solarview.com.br>
2026-08-06 23:42:03 -0300

    📌 Version: bump app version

--- src/constants/copyright.ts
+++ src/constants/copyright.ts
@@ -2,10 +2,10 @@ import type { TypeCopyright } from "../types/copyright";
 
 export const COPYRIGHT: TypeCopyright = {
   year: 2025,
-  version: "1.0.9",
+  version: "1.0.10",
   lastName: "Moura",
   firstName: "Pablo",
   developedBy: "Pablo Francisco Moura",
-  lastUpdated: "05/08/2026",
+  lastUpdated: "06/08/2026",
   fullLastName: "Francisco Moura",
 };
