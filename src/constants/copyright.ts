3d9ca002147bd55965aa0e20aa3a05d80091f8f8
Pablo Moura <pablo.f.moura17@gmail.com>
2026-09-18 20:22:28 -0300

    📌 Version: bump app version "1.1.1"

--- src/constants/copyright.ts
+++ src/constants/copyright.ts
@@ -2,7 +2,7 @@ import type { TypeCopyright } from "../types/copyright";
 
 export const COPYRIGHT: TypeCopyright = {
   year: 2025,
-  version: "1.1.0",
+  version: "1.1.1",
   lastName: "Moura",
   firstName: "Pablo",
   developedBy: "Pablo Francisco Moura",
